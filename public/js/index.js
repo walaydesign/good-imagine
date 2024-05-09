da04ff9549d220af242fa0d1c3235d84a72c3ec6
youzhen <yl301114@gmail.com>
2024-05-09 13:43:24 +0100

    調整

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,6 @@
+AOS.init({startEvent: 'load'});
+window.addEventListener('load', AOS.refresh);
+
 var introSwiperSmall_1 = new Swiper(".intro_swiper-small-1", {
     slidesPerView: 3,
     speed: 800,
@@ -25,8 +28,8 @@ var introSwiperSmall_2 = new Swiper(".intro_swiper-small-2", {
 var introSwiper_2 = new Swiper(".intro_swiper-2", {
     slidesPerView: 1,
     navigation: {
-      nextEl: ".intro_arrow-1-next",
-      prevEl: ".intro_arrow-1-prev",
+      nextEl: ".intro_arrow-2-next",
+      prevEl: ".intro_arrow-2-prev",
     },
     speed: 800,
     thumbs: {
@@ -53,3 +56,28 @@ $(".layout-popup_bg").click(function() {
     $(".layout-popup").fadeOut(300);
     $(".layout-popup_pic").hide();
 })
+
+$(".btn-top").click(function(){
+    $("html, body").animate({scrollTop: 0},300);
+})
+
+$(".btn-contact").click(function() {
+    let contact = $("#contact").offset().top;
+    $("html, body").animate({scrollTop: contact},300);
+})
+
+function sendEmail(){
+    Email.send({
+        SecureToken: "8207c7d7-4a6c-4797-870d-d16ee71023ce",
+        // To : "ryanlin9001@gmail.com, zhulixdesign@gmail.com, zhuli705098@gmail.com",
+        To : "yl301114@gmail.com",
+        From : "walayydesign@gmail.com",
+        Subject : "印象好好預約賞屋",
+        Body : "姓名:" + document.getElementById("name").value
+                + "<br>電話:" + document.getElementById("phone").value
+                + "<br>Line ID:" + document.getElementById("phone").value
+                + "<br>現居市區:" + document.getElementById("city").value
+    }).then(
+        message => alert("感謝您的來信！我們很快就會和您聯繫！"),
+    );
+}
